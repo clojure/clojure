2023e999fa999c25acbbb212a9197eed6dce956c
Rich Hickey <richhickey@gmail.com>
2006-06-07 19:21:05 +0000

    ported PersistentArrayIdentityMap

---
+++ src/cli/runtime/PersistentArrayIdentityMap.cs
@@ -0,0 +1,30 @@
+/**
+ *   Copyright (c) Rich Hickey. All rights reserved.
+ *   The use and distribution terms for this software are covered by the
+ *   Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+ *   which can be found in the file CPL.TXT at the root of this distribution.
+ *   By using this software in any fashion, you are agreeing to be bound by
+ * 	 the terms of this license.
+ *   You must not remove this notice, or any other, from this software.
+ **/
+
+using System;
+
+namespace org.clojure.runtime
+{
+/**
+ * ArrayMap using identity (==) comparison instead of equals
+ */
+public class PersistentArrayIdentityMap : PersistentArrayMap {
+public PersistentArrayIdentityMap() {
+}
+
+public PersistentArrayIdentityMap(Object[] init) :base(init) {
+}
+
+bool equalKey(Object k1, Object k2) {
+    return k1 == k2;
+}
+}
+
+}
