ec3c8efd317790935c2859300031281e05d41e4a
Rich Hickey <richhickey@gmail.com>
2006-06-07 18:36:20 +0000

    added IPersistentMap, IMapEntry

---
+++ src/cli/runtime/IPersistentMap.cs
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Rich Hickey. All rights reserved.
+ * The use and distribution terms for this software are covered by the
+ * Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+ * which can be found in the file CPL.TXT at the root of this distribution.
+ * By using this software in any fashion, you are agreeing to be bound by
+ * the terms of this license.
+ * You must not remove this notice, or any other, from this software.
+ */
+
+using System;
+using System.Collections;
+
+namespace org.clojure.runtime
+{
+
+public interface IPersistentMap :  IEnumerable{
+
+int count();
+
+bool contains(Object key);
+
+IMapEntry find(Object key);
+
+IPersistentMap add(Object key);
+
+IPersistentMap put(Object key, Object val);
+
+IPersistentMap remove(Object key);
+
+Object get(Object key);
+
+int capacity();
+}
+
+}
