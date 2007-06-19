5eeba526b27ced81a6116ac31d989830d589b456
Rich Hickey <richhickey@gmail.com>
2007-06-19 14:20:34 +0000

    Seq/Iterator interop

---
+++ src/jvm/clojure/lang/SeqIterator.java
@@ -0,0 +1,38 @@
+/**
+ *   Copyright (c) Rich Hickey. All rights reserved.
+ *   The use and distribution terms for this software are covered by the
+ *   Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+ *   which can be found in the file CPL.TXT at the root of this distribution.
+ *   By using this software in any fashion, you are agreeing to be bound by
+ * 	 the terms of this license.
+ *   You must not remove this notice, or any other, from this software.
+ **/
+
+/* rich Jun 19, 2007 */
+
+package clojure.lang;
+
+import java.util.Iterator;
+
+public class SeqIterator implements Iterator{
+
+ISeq seq;
+
+public SeqIterator(ISeq seq){
+	this.seq = seq;
+}
+
+public boolean hasNext(){
+	return seq != null;
+}
+
+public Object next(){
+	Object ret = RT.first(seq);
+	seq = RT.rest(seq);
+	return ret;
+}
+
+public void remove(){
+throw new UnsupportedOperationException();
+}
+}
