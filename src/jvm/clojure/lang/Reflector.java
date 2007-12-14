981cda7c8e8dec5e3343fcd95e3bb7cf12921cdc
Rich Hickey <richhickey@gmail.com>
2007-12-14 16:44:54 +0000

    removed reflective static class pseudo-field

--- src/jvm/clojure/lang/Reflector.java
+++ src/jvm/clojure/lang/Reflector.java
@@ -147,8 +147,8 @@ public static Object invokeStaticMethod(String className, String methodName, Obj
 
 public static Object getStaticField(String className, String fieldName) throws Exception{
 	Class c = Class.forName(className);
-	if(fieldName.equals("class"))
-		return c;
+//	if(fieldName.equals("class"))
+//		return c;
 	Field f = getField(c, fieldName, true);
 	if(f != null)
 		{
