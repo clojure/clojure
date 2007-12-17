742bf71003d62c9d41881aa7c59740993a749243
Rich Hickey <richhickey@gmail.com>
2007-12-17 14:43:43 +0000

    fixed pop when count 2

--- src/jvm/clojure/lang/PersistentVector.java
+++ src/jvm/clojure/lang/PersistentVector.java
@@ -324,7 +324,7 @@ public PersistentVector pop(){
 		{
 		return (PersistentVector) EMPTY.withMeta(meta());
 		}
-	if(newroot.length == 1)
+	if(shift > 0 && newroot.length == 1)
 		{
 		newroot = (Object[]) newroot[0];
 		newshift -= 5;
