0f234b3967cea2450ec86b6e21f17642e12ea939
Rich Hickey <richhickey@gmail.com>
2007-12-18 12:55:15 +0000

    Made implementing void-returning methods optional

--- src/jvm/clojure/lang/ProxyHandler.java
+++ src/jvm/clojure/lang/ProxyHandler.java
@@ -25,11 +25,15 @@ public ProxyHandler(IPersistentMap fns){
 }
 
 public Object invoke(Object proxy, Method method, Object[] args) throws Throwable{
+	Class rt = method.getReturnType();
 	IFn fn = (IFn) fns.valAt(method.getName());
 	if(fn == null)
+		{
+		if(rt == Void.TYPE)
+			return null;
 		throw new UnsupportedOperationException();
+		}
 	Object ret = fn.applyTo(ArraySeq.create(args));
-	Class rt = method.getReturnType();
 	if(rt == Void.TYPE)
 		return null;
 	else if(rt.isPrimitive())
@@ -37,19 +41,19 @@ public Object invoke(Object proxy, Method method, Object[] args) throws Throwabl
 		if(rt == Character.TYPE)
 			return ret;
 		else if(rt == Integer.TYPE)
-			return ((Number)ret).intValue();
+			return ((Number) ret).intValue();
 		else if(rt == Long.TYPE)
-			return ((Number)ret).longValue();
+			return ((Number) ret).longValue();
 		else if(rt == Float.TYPE)
-			return ((Number)ret).floatValue();
+			return ((Number) ret).floatValue();
 		else if(rt == Double.TYPE)
-			return ((Number)ret).doubleValue();
-		else if(rt  == Boolean.TYPE)
-			return ret == null?Boolean.FALSE:Boolean.TRUE;
+			return ((Number) ret).doubleValue();
+		else if(rt == Boolean.TYPE)
+			return ret == null ? Boolean.FALSE : Boolean.TRUE;
 		else if(rt == Byte.TYPE)
-			return (byte)((Number)ret).intValue();
+			return (byte) ((Number) ret).intValue();
 		else if(rt == Short.TYPE)
-			return (short)((Number)ret).intValue();
+			return (short) ((Number) ret).intValue();
 		}
 	return ret;
 }
