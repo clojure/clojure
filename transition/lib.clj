a7eaa3f1e012e0f228771cd047984ee8b202e326
scgilardi <scgilardi@gmail.com>
2008-08-19 06:20:24 +0000

    transition/lib.clj: refine comments

--- transition/lib.clj
+++ transition/lib.clj
@@ -301,9 +301,9 @@
   @*loaded-libs*)
 
 (defn load-resources
-  "Loads Clojure code from Java resources in classpath. A path is
-  interpreted as classpath-relative if it begins with a slash or relative
-  to the root directory for the current namespace otherwise."
+  "Loads Clojure code from resources in classpath. A path is interpreted as
+  classpath-relative if it begins with a slash or relative to the root
+  directory for the current namespace otherwise."
   [& paths]
   (doseq path paths
     (let [path (if (.startsWith path "/")
