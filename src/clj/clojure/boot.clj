4b58241b944a261e708288bdaa520f549e0e259c
Rich Hickey <richhickey@gmail.com>
2008-08-28 18:17:22 +0000

    added defonce

--- src/clj/clojure/boot.clj
+++ src/clj/clojure/boot.clj
@@ -2984,3 +2984,11 @@
      (in-ns '~name)
      (when-not existed#
        (clojure/refer '~'clojure))))
+
+(defmacro defonce 
+  "defs name to have the root value of the expr iff the named var has no root value, 
+  else expr is unevaluated"
+  [name expr]
+  `(let [v# (def ~name)]
+     (when-not (.hasRoot v#)
+       (def ~name ~expr))))
