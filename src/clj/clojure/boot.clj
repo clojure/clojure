6bb0a315870e33237a484cb9062e51ea81aa76a5
Rich Hickey <richhickey@gmail.com>
2008-09-18 00:49:28 +0000

    Fixed doc for descendants

--- src/clj/clojure/boot.clj
+++ src/clj/clojure/boot.clj
@@ -2830,10 +2830,11 @@
                 ta)))))
 
 (defn descendants
-  "Returns the immediate and indirect children of tag, either via a Java type
-  inheritance relationship or a relationship established via derive. h
-  must be a hierarchy obtained from make-hierarchy, if not supplied
-  defaults to the global hierarchy" 
+  "Returns the immediate and indirect children of tag, through a
+  relationship established via derive. h must be a hierarchy obtained
+  from make-hierarchy, if not supplied defaults to the global
+  hierarchy. Note: does not work on Java type inheritance
+  relationships."
   ([tag] (descendants global-hierarchy tag))
   ([h tag] (if (class? tag)
              (throw (java.lang.UnsupportedOperationException. "Can't get descendants of classes"))
