7cfb20f6a9bedd400eb1552ec34f01c7e2db2f1c
Stuart Sierra <mail@stuartsierra.com>
2008-12-15 21:42:40 +0000

    Stuart Sierra: Moved libs I wrote to Eclipse Public License 1.0

--- src/clojure/contrib/test_is.clj
+++ src/clojure/contrib/test_is.clj
@@ -6,10 +6,10 @@
 ;; Thanks to Chas Emerick, Allen Rohner, and Stuart Halloway for
 ;; contributions and suggestions.
 
-;; Copyright (c) 2008 Stuart Sierra. All rights reserved.  The use and
-;; distribution terms for this software are covered by the Common
-;; Public License 1.0 (http://www.opensource.org/licenses/cpl1.0.php)
-;; which can be found in the file CPL.TXT at the root of this
+;; Copyright (c) Stuart Sierra, 2008. All rights reserved.  The use
+;; and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this
 ;; distribution.  By using this software in any fashion, you are
 ;; agreeing to be bound by the terms of this license.  You must not
 ;; remove this notice, or any other, from this software.
