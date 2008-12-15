dfd1a7a26084706b512ca50eef838e64c6f99aa2
Chouser <chouser@n01se.net>
2008-12-15 04:54:21 +0000

    Switch my libs to EPL

--- src/clojure/contrib/gen_interface.clj
+++ src/clojure/contrib/gen_interface.clj
@@ -1,7 +1,7 @@
 ;   Copyright (c) Chris Houser, July 2008. All rights reserved.
 ;   The use and distribution terms for this software are covered by the
-;   Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
-;   which can be found in the file CPL.TXT at the root of this distribution.
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
 ;   By using this software in any fashion, you are agreeing to be bound by
 ;   the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
--- src/clojure/contrib/lazy_xml/with_pull.clj
+++ src/clojure/contrib/lazy_xml/with_pull.clj
@@ -1,3 +1,13 @@
+;   Copyright (c) Chris Houser, Dec 2008. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+; optional module to allow lazy-xml to use pull parser instead of sax
+
 (in-ns 'clojure.contrib.lazy-xml)
 (import '(org.xmlpull.v1 XmlPullParser XmlPullParserFactory))
 
