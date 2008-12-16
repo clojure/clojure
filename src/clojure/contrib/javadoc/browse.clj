5b151fe8d40a67a79e2c97fce5fd36d121853c2f
Christophe Grand <christophe@cgrand.net>
2008-12-16 16:13:09 +0000

    moved to EPL

--- src/clojure/contrib/javadoc/browse.clj
+++ src/clojure/contrib/javadoc/browse.clj
@@ -2,12 +2,12 @@
 
 ;   Copyright (c) Christophe Grand, December 2008. All rights reserved.
 ;   The use and distribution terms for this software are covered by the
-;   Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
-;   which can be found in the file CPL.TXT at the root of this distribution.
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this 
+;   distribution.
 ;   By using this software in any fashion, you are agreeing to be bound by
 ;   the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
-
 
 (ns clojure.contrib.javadoc.browse
     (:import (java.net URI)))
