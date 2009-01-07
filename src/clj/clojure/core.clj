03e7ecc9e7d6cd625af6846c0937e3c86e1ca11a
Rich Hickey <richhickey@gmail.com>
2009-01-07 17:22:48 +0000

    removed declare of atom and swap!

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -3681,8 +3681,6 @@
        ~@body
        (recur))))
 
-(declare atom swap!)
-
 (defn memoize 
   "Returns a memoized version of a referentially transparent function. The
   memoized version of the function keeps a cache of the mapping from arguments
