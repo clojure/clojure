b78dd0739319e226a793a8986682dbe8db844ccd
Konrad Hinsen <konrad.hinsen@laposte.net>
2009-01-08 09:32:40 +0000

    New file macros.clj (plus a corresponding entry in build.xml)

---
+++ src/clojure/contrib/macros.clj
@@ -0,0 +1,25 @@
+;; Various useful macros
+
+;; Copyright (c) Konrad Hinsen, 2009. All rights reserved.  The use
+;; and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this
+;; distribution.  By using this software in any fashion, you are
+;; agreeing to be bound by the terms of this license.  You must not
+;; remove this notice, or any other, from this software.
+
+(ns clojure.contrib.macros)
+
+(defmacro const
+  "Evaluate the constant expression expr at compile time."
+  [expr]
+  (eval expr))
+
+(defmacro letfn
+  "A variant of let for local function definitions. fn-bindings consists
+   of name/args/body triples, with (letfn [name args body] ...)
+   being equivalent to (let [name (fn name args body)] ...)."
+  [fn-bindings exprs]
+  (let [makefn (fn [[name args body]] (list name (list 'fn name args body)))
+	fns (vec (apply concat (map makefn (partition 3 fn-bindings))))]
+  `(let ~fns ~exprs)))
