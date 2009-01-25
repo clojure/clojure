a36aa739efe642715ebf1c669281afc9ede946c6
Stuart Halloway <stu@thinkrelevance.com>
2009-01-25 19:52:46 +0000

    added test_contrib.clj, similar test_clojure.clj added test_clojure, test_contrib, and test tasks to build.xml

---
+++ src/clojure/contrib/test_contrib.clj
@@ -0,0 +1,27 @@
+;;  Copyright (c) Stuart Halloway. All rights reserved.  The use and
+;;  distribution terms for this software are covered by the Eclipse Public
+;;  License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can
+;;  be found in the file epl-v10.html at the root of this distribution.  By
+;;  using this software in any fashion, you are agreeing to be bound by the
+;;  terms of this license.  You must not remove this notice, or any other,
+;;  from this software.
+;;
+;;  clojure.contrib.test-contrib
+;;
+;;  Tests for the facilities provided by clojure-contrib
+;;
+;;  stuart.halloway (gmail)
+
+(ns clojure.contrib.test-contrib
+  (:use clojure.contrib.test-is))
+
+(def tests [:str-utils])
+
+(defn test-name
+  [test]
+  (symbol (str "clojure.contrib.test-contrib." (name test))))
+
+(doseq [test tests]
+  (require (test-name test)))
+
+(apply run-tests (map test-name tests))
