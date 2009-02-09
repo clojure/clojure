3586754272ab23f014acf5f3aab3e8ce8202fc00
Rich Hickey <richhickey@gmail.com>
2009-02-09 17:16:42 +0000

    fixed Ratio->bigdec, patch from jbondeson

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -1894,6 +1894,7 @@
   [x] (cond
        (decimal? x) x
        (float? x) (. BigDecimal valueOf (double x))
+       (ratio? x) (/ (BigDecimal. (.numerator x)) (.denominator x))
        (instance? BigInteger x) (BigDecimal. #^BigInteger x)
        (number? x) (BigDecimal/valueOf (long x))
        :else (BigDecimal. x)))
