61e7ce6d189246be05d4f612d9f8a5bd36150591
Jeffrey Straszheim <straszheimjeffrey@gmail.com>
2009-02-24 02:16:25 +0000

    Fixed copyright notice (oops)

--- src/clojure/contrib/graph.clj
+++ src/clojure/contrib/graph.clj
@@ -1,4 +1,4 @@
-;;  Copyright (c) Stephen C. Gilardi. All rights reserved.  The use and
+;;  Copyright (c) Jeffrey Straszheim. All rights reserved.  The use and
 ;;  distribution terms for this software are covered by the Eclipse Public
 ;;  License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can
 ;;  be found in the file epl-v10.html at the root of this distribution.  By
