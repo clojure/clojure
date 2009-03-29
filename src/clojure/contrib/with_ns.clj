2166ff44388e992b8671f7a49349a54f213b4dfb
Stuart Sierra <mail@stuartsierra.com>
2009-03-29 03:17:03 +0000

    with_ns.clj: added with-temp-ns, fixed handling of refer/use

--- src/clojure/contrib/with_ns.clj
+++ src/clojure/contrib/with_ns.clj
@@ -19,4 +19,16 @@
   object or a symbol.  This makes it possible to define functions in
   namespaces other than the current one."
   [ns & body]
-  `(binding [*ns* (the-ns ~ns)] (eval '(do ~@body))))
+  `(binding [*ns* (the-ns ~ns)]
+     ~@(map (fn [form] `(eval '~form)) body)))
+
+(defmacro with-temp-ns
+  "Evaluates body in an anonymous namespace, which is then immediately
+  removed.  The temporary namespace will 'refer' clojure.core."
+  [& body]
+  `(do (create-ns 'sym#)
+       (let [result# (with-ns 'sym#
+                      (clojure.core/refer-clojure)
+                      ~@body)]
+         (remove-ns 'sym#)
+         result#)))
