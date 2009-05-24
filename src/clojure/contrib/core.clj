d9049bf729b37cd6d21022674d966bec31bcb7a7
scgilardi <scgilardi@gmail.com>
2009-05-24 00:25:49 +0000

    clojure.contrib.core: add dissoc-in

--- src/clojure/contrib/core.clj
+++ src/clojure/contrib/core.clj
@@ -42,3 +42,22 @@
    (.?. nil .toUpperCase (.substring 1)) returns nil
    "
    .. .?.)
+
+;; ----------------------------------------------------------------------
+;; scgilardi at gmail
+
+(defn dissoc-in
+  "Dissociates an entry from a nested associative structure returning a new
+  nested structure. keys is a sequence of keys. Any empty maps that result
+  will not be present in the new structure."
+  [m [k & ks :as keys]]
+  (if ks
+    (if-let [nextmap (get m k)]
+      (let [newmap (dissoc-in nextmap ks)]
+        (if (seq newmap)
+          (assoc m k newmap)
+          (dissoc m k)))
+      m)
+    (dissoc m k)))
+
+;; ----------------------------------------------------------------------
