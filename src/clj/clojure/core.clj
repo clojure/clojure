e0e8326871983be5615f5c0bc9dbf66140c7017f
Dimitry Gashinsky <dimi...@gashinsky.com>
2009-06-20 09:38:31 -0400

    add optional pad argument to partition. Fixes #120
    Signed-off-by: Chouser <chouser@n01se.net>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -1713,15 +1713,24 @@
 (defn partition
   "Returns a lazy sequence of lists of n items each, at offsets step
   apart. If step is not supplied, defaults to n, i.e. the partitions
-  do not overlap."
+  do not overlap. If a pad collection is supplied, use its elements as
+  necessary to complete last partition upto n items. In case there are
+  not enough padding elements, return a partition with less than n items."
   ([n coll]
      (partition n n coll))
   ([n step coll]
-   (lazy-seq
-    (when-let [s (seq coll)]
-      (let [p (take n s)]
-        (when (= n (count p))
-          (cons p (partition n step (drop step s)))))))))
+     (lazy-seq
+       (when-let [s (seq coll)]
+         (let [p (take n s)]
+           (when (= n (count p))
+             (cons p (partition n step (drop step s))))))))
+  ([n step pad coll]
+     (lazy-seq
+       (when-let [s (seq coll)]
+         (let [p (take n s)]
+           (if (= n (count p))
+             (cons p (partition n step pad (drop step s)))
+             (list (take n (concat p pad)))))))))
 
 ;; evaluation
 
