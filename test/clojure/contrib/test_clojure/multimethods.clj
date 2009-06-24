d5f4c4cb9e406236e6aa8c0f1e65b7dcd0bf35af
Stuart Halloway <stu@thinkrelevance.com>
2009-06-24 00:29:41 -0400

    gtic work-in-progress:   test libraries moved over   tests moved over   build broken (next commits will break dependencies)

---
+++ test/clojure/contrib/test_clojure/multimethods.clj
@@ -0,0 +1,25 @@
+;;  Copyright (c) Frantisek Sodomka. All rights reserved.  The use and
+;;  distribution terms for this software are covered by the Eclipse Public
+;;  License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can
+;;  be found in the file epl-v10.html at the root of this distribution.  By
+;;  using this software in any fashion, you are agreeing to be bound by the
+;;  terms of this license.  You must not remove this notice, or any other,
+;;  from this software.
+
+(ns clojure.contrib.test-clojure.multimethods
+  (:use clojure.contrib.test-is))
+
+; http://clojure.org/multimethods
+
+; defmulti
+; defmethod
+; remove-method
+; prefer-method
+; methods
+; prefers
+
+; derive, [underive]
+; isa?
+; parents, ancestors, descendants
+; make-hierarchy
+
