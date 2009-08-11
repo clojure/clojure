47df6056bf9cd622b22175e114d9e26eb31d2742
Christophe Grand <christophe@cgrand.net>
2009-08-11 10:51:30 +0200

    in transient mode, when nesting a hash collision node in a bitmap node, use a better sized initial array
    Signed-off-by: Rich Hickey <richhickey@gmail.com>

--- src/jvm/clojure/lang/PersistentHashMap.java
+++ src/jvm/clojure/lang/PersistentHashMap.java
@@ -839,7 +839,7 @@ final static class HashCollisionNode implements INode{
 			return ensureEditable(edit, count + 1, newArray);
 		}
 		// nest it in a bitmap node
-		return new BitmapIndexedNode(edit, bitpos(this.hash, shift), new Object[] {null, this})
+		return new BitmapIndexedNode(edit, bitpos(this.hash, shift), new Object[] {null, this, null, null})
 			.assoc(edit, shift, hash, key, val, addedLeaf);
 	}	
 
