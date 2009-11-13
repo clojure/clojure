841dc9c9df717ee665b95e9471dc5e01b5c113eb
Rich Hickey <richhickey@gmail.com>
2009-11-13 06:50:10 -0500

    fix some's doc string

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -1717,7 +1717,7 @@
 (defn some
   "Returns the first logical true value of (pred x) for any x in coll,
   else nil.  One common idiom is to use a set as pred, for example
-  this will return true if :fred is in the sequence, otherwise nil:
+  this will return :fred if :fred is in the sequence, otherwise nil:
   (some #{:fred} coll)"
   [pred coll]
     (when (seq coll)
