d855c45b035921150a3698d63d2b2b8852e11311
John Szakmeister <john@szakmeister.net>
2010-10-23 05:02:54 -0400

    Fix #103: incorrect error with if-let.

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -1622,7 +1622,8 @@
    `(if-let ~bindings ~then nil))
   ([bindings then else & oldform]
    (assert-args if-let
-     (and (vector? bindings) (nil? oldform)) "a vector for its binding"
+     (vector? bindings) "a vector for its binding"
+     (nil? oldform) "1 or 2 forms to be passed"
      (= 2 (count bindings)) "exactly 2 forms in binding vector")
    (let [form (bindings 0) tst (bindings 1)]
      `(let [temp# ~tst]
