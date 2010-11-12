8bb9789f0ff510ccb0b48891923a1e5148bdfe69
Alex Redington <alex.redington@thinkrelevance.com>
2010-11-12 13:21:34 -0500

    Updated the docstring on slurp to indicate it accepts an arbitrary clojure.java.io/reader
    Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -5798,8 +5798,8 @@
     opts))
 
 (defn slurp
-  "Reads the file named by f using the encoding enc into a string
-  and returns it."
+  "Opens a reader on f and reads all its contents, returning a string.
+  See clojure.java.io/reader for a complete list of supported arguments."
   {:added "1.0"}
   ([f & opts]
      (let [opts (normalize-slurp-opts opts)
