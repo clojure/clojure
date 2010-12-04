eb076dab7b7be68957922f47ad8d140e4ec579a1
Rich Hickey <richhickey@gmail.com>
2010-12-04 09:48:18 -0500

    inline nil?

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -398,7 +398,8 @@
   "Returns true if x is nil, false otherwise."
   {:tag Boolean
    :added "1.0"
-   :static true}
+   :static true
+   :inline (fn [x] (list 'clojure.lang.Util/identical x nil))}
   [x] (clojure.lang.Util/identical x nil))
 
 (def
