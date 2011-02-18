e288fcc686f1bf81f15c656a956c5b06a44ac39e
Stuart Sierra <mail@stuartsierra.com>
2011-02-18 10:18:56 -0500

    CLJ-739 Ensure version.properties is closed when loading core.clj
    Signed-off-by: Stuart Halloway <stu@Stuart-Halloways-MacBook-Air.local>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -5918,10 +5918,11 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; clojure version number ;;;;;;;;;;;;;;;;;;;;;;
 
-(let [version-stream (.getResourceAsStream
-		      (clojure.lang.RT/baseLoader) 
-		      "clojure/version.properties")
-      properties     (doto (new java.util.Properties) (.load version-stream))
+(let [properties (with-open [version-stream (.getResourceAsStream
+                                             (clojure.lang.RT/baseLoader)
+                                             "clojure/version.properties")]
+                   (doto (new java.util.Properties)
+                     (.load version-stream)))
       version-string (.getProperty properties "version")
       [_ major minor incremental qualifier snapshot]
       (re-matches
