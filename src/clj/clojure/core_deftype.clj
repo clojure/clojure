8ed6e82d4d85e682ecc49e6b63a135980a531bfb
Rich Hickey <richhickey@gmail.com>
2011-05-12 18:10:12 -0400

    get rid of spurious tags on deftype/defrecord

--- src/clj/clojure/core_deftype.clj
+++ src/clj/clojure/core_deftype.clj
@@ -135,8 +135,7 @@
   "Do not use this directly - use defrecord"
   {:added "1.2"}
   [tagname name fields interfaces methods]
-  (let [tag (keyword (str *ns*) (str tagname))
-        classname (with-meta (symbol (str (namespace-munge *ns*) "." name)) (meta name))
+  (let [classname (with-meta (symbol (str (namespace-munge *ns*) "." name)) (meta name))
         interfaces (vec interfaces)
         interface-set (set (map resolve interfaces))
         methodname-set (set (map first methods))
@@ -293,7 +292,6 @@
   (let [gname name
         [interfaces methods opts] (parse-opts+specs opts+specs)
         classname (symbol (str (namespace-munge *ns*) "." gname))
-        tag (keyword (str *ns*) (str name))
         hinted-fields fields
         fields (vec (map #(with-meta % nil) fields))]
     `(let []
@@ -385,7 +383,6 @@
   (let [gname name
         [interfaces methods opts] (parse-opts+specs opts+specs)
         classname (symbol (str (namespace-munge *ns*) "." gname))
-        tag (keyword (str *ns*) (str name))
         hinted-fields fields
         fields (vec (map #(with-meta % nil) fields))]
     `(let []
