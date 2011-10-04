3fde8b9ebcf32a43017a28648ed6c9d3bfc128db
Rich Hickey <richhickey@gmail.com>
2011-10-04 14:30:18 -0400

    test for sentinel with identical?, not =

--- src/clj/clojure/main.clj
+++ src/clj/clojure/main.clj
@@ -262,7 +262,7 @@
      (flush)
      (loop []
        (when-not 
-       	 (try (= (read-eval-print) request-exit)
+       	 (try (identical? (read-eval-print) request-exit)
 	  (catch Throwable e
 	   (caught e)
 	   (set! *e e)
