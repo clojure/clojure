e38137386713ea430141cb07bf6d954e63b6357f
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2011-10-14 20:05:29 +0200

    CLJ-855: Util.sneakyThrow() throws any exception without being required to declare or catch it
    Signed-off-by: Rich Hickey <richhickey@gmail.com>

--- src/jvm/clojure/lang/Util.java
+++ src/jvm/clojure/lang/Util.java
@@ -179,4 +179,24 @@ static public RuntimeException runtimeException(Throwable e){
 	return new RuntimeException(e);
 }
 
+/**
+ * Throw even checked exceptions without being required
+ * to declare them or catch them. Suggested idiom:
+ * <p>
+ * <code>throw sneakyThrow( some exception );</code>
+ */
+static public RuntimeException sneakyThrow(Throwable t) {
+    // http://www.mail-archive.com/javaposse@googlegroups.com/msg05984.html
+	if (t == null)
+		throw new NullPointerException();
+	Util.<RuntimeException>sneakyThrow0(t);
+	return null;
 }
+
+@SuppressWarnings("unchecked")
+static private <T extends Throwable> void sneakyThrow0(Throwable t) throws T {
+	throw (T) t;
+}
+
+}
+
