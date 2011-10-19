8da80d4c4dab56e32babdf0e3af48467791bb7f7
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2011-10-19 19:42:21 +0200

    CLJ-854: document that (flatten nil) returns an empty sequence
    Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -6252,7 +6252,7 @@
 (defn flatten
   "Takes any nested combination of sequential things (lists, vectors,
   etc.) and returns their contents as a single, flat sequence.
-  (flatten nil) returns nil."
+  (flatten nil) returns an empty sequence."
   {:added "1.2"
    :static true}
   [x]
