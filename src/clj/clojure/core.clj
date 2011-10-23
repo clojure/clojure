b4a2216d78173bb81597f267b6025c74a508bd03
Rich Hickey <richhickey@gmail.com>
2011-10-23 15:19:11 -0400

    switch core/hash to use hasheq

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -4533,10 +4533,13 @@
 
 
 (defn hash
-  "Returns the hash code of its argument"
+  "Returns the hash code of its argument. Note this is the hash code
+  consistent with =, and thus is different than .hashCode for Integer,
+  Short, Byte and Clojure collections."
+
   {:added "1.0"
    :static true}
-  [x] (. clojure.lang.Util (hash x)))
+  [x] (. clojure.lang.Util (hasheq x)))
 
 (defn interpose
   "Returns a lazy seq of the elements of coll separated by sep"
