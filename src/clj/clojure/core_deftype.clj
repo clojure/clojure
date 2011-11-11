8a0482d351be66d63edc83e29c7b3e7aa691a886
Kurt Harriger <kurtharriger@gmail.com>
2011-11-11 11:46:17 -0500

    declare record factory functions for use within defrecord implementation
    Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

--- src/clj/clojure/core_deftype.clj
+++ src/clj/clojure/core_deftype.clj
@@ -325,6 +325,8 @@
         hinted-fields fields
         fields (vec (map #(with-meta % nil) fields))]
     `(let []
+       (declare ~(symbol (str  '-> gname)))
+       (declare ~(symbol (str 'map-> gname)))
        ~(emit-defrecord name gname (vec hinted-fields) (vec interfaces) methods)
        (import ~classname)
        ~(build-positional-factory gname classname fields)
