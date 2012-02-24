15bd255be000e6daa33795a967b6a44ee471afe9
David McNeil <dmcneil@revelytix.com>
2012-02-24 09:29:46 -0800

    await-for return value doc fix
    Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -2879,8 +2879,8 @@
 (defn await-for
   "Blocks the current thread until all actions dispatched thus
   far (from this thread or agent) to the agents have occurred, or the
-  timeout (in milliseconds) has elapsed. Returns nil if returning due
-  to timeout, non-nil otherwise."
+  timeout (in milliseconds) has elapsed. Returns logical false if
+  returning due to timeout, logical true otherwise."
   {:added "1.0"
    :static true}
   [timeout-ms & agents]
