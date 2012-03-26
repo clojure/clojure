d8be6135cabc981f7043efe5fd7a386a3faf3bd2
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2012-03-26 15:29:04 -0700

    Clarify doc strings of sort and sort-by: they will modify Java array arguments
    Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -2690,8 +2690,9 @@
 
 (defn sort
   "Returns a sorted sequence of the items in coll. If no comparator is
-  supplied, uses compare. comparator must
-  implement java.util.Comparator."
+  supplied, uses compare.  comparator must implement
+  java.util.Comparator.  If coll is a Java array, it will be modified.
+  To avoid this, sort a copy of the array."
   {:added "1.0"
    :static true}
   ([coll]
@@ -2706,8 +2707,9 @@
 (defn sort-by
   "Returns a sorted sequence of the items in coll, where the sort
   order is determined by comparing (keyfn item).  If no comparator is
-  supplied, uses compare. comparator must
-  implement java.util.Comparator."
+  supplied, uses compare.  comparator must implement
+  java.util.Comparator.  If coll is a Java array, it will be modified.
+  To avoid this, sort a copy of the array."
   {:added "1.0"
    :static true}
   ([keyfn coll]
