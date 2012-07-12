a4c22960636a30861e2dc82c42be5a967f8181a4
Gabriel Horner <gabriel.horner@gmail.com>
2012-07-12 10:42:50 -0700

    Correct misspelling in ns-resolve doc string
    Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -3876,7 +3876,7 @@
 
 (defn ns-resolve
   "Returns the var or Class to which a symbol will be resolved in the
-  namespace (unless found in the environement), else nil.  Note that
+  namespace (unless found in the environment), else nil.  Note that
   if the symbol is fully qualified, the var/Class to which it resolves
   need not be present in the namespace."
   {:added "1.0"
