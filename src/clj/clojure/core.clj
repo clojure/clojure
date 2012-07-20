9458266b8d8693e0ec0ef7d5e1697390006a7023
Devin Walters <devinw@gmail.com>
2012-07-20 18:04:50 -0400

    Fix inconsistent range, refs #1018
    Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -2650,10 +2650,11 @@
    :static true}
   [f x] (cons x (lazy-seq (iterate f (f x)))))
 
-(defn range 
+(defn range
   "Returns a lazy seq of nums from start (inclusive) to end
-  (exclusive), by step, where start defaults to 0, step to 1, and end
-  to infinity."
+  (exclusive), by step, where start defaults to 0, step to 1, and end to
+  infinity. When step is equal to 0, returns an infinite sequence of
+  start. When start is equal to end, returns empty list."
   {:added "1.0"
    :static true}
   ([] (range 0 Double/POSITIVE_INFINITY 1))
@@ -2662,7 +2663,9 @@
   ([start end step]
    (lazy-seq
     (let [b (chunk-buffer 32)
-          comp (if (pos? step) < >)]
+          comp (cond (or (zero? step) (= start end)) not=
+                     (pos? step) <
+                     (neg? step) >)]
       (loop [i start]
         (if (and (< (count b) 32)
                  (comp i end))
