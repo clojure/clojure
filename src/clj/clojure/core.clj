54f790ec90f6044c4e16be4ecb7cb7b3396d9641
Colin Jones <trptcolin@gmail.com>
2012-08-07 16:44:15 -0500

    Update docstring for deliver's actual behavior
    Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -6340,7 +6340,7 @@
 (defn deliver
   "Alpha - subject to change.
   Delivers the supplied value to the promise, releasing any pending
-  derefs. A subsequent call to deliver on a promise will throw an exception."
+  derefs. A subsequent call to deliver on a promise will have no effect."
   {:added "1.1"
    :static true}
   [promise val] (promise val))
