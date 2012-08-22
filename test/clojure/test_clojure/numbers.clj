7f69246f17a559119fbad8747100bb7586ff6e92
Stuart Halloway <stu@thinkrelevance.com>
2012-08-21 23:18:34 -0400

    use test.generative's runner, add a few generative tests
    Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

--- test/clojure/test_clojure/numbers.clj
+++ test/clojure/test_clojure/numbers.clj
@@ -13,7 +13,9 @@
 
 (ns clojure.test-clojure.numbers
   (:use clojure.test
-        clojure.template))
+        [clojure.test.generative :exclude (is)]
+        clojure.template)
+  (:require [clojure.test.generative.generators :as gen]))
 
 
 ; TODO:
@@ -563,3 +565,77 @@ Math/pow overflows to Infinity."
            min
            max))))
 
+(defn integer
+  "Distribution of integers biased towards the small, but
+   including all longs."
+  []
+  (gen/one-of #(gen/uniform -1 32) gen/byte gen/short gen/int gen/long))
+
+(defn longable?
+  [n]
+  (try
+   (long n)
+   true
+   (catch Exception _)))
+
+(defspec integer-commutative-laws
+  (partial map identity)
+  [^{:tag `integer} a ^{:tag `integer} b]
+  (if (longable? (+' a b))
+    (assert (= (+ a b) (+ b a)
+               (+' a b) (+' b a)
+               (unchecked-add a b) (unchecked-add b a)))
+    (assert (= (+' a b) (+' b a))))
+  (if (longable? (*' a b))
+    (assert (= (* a b) (* b a)
+               (*' a b) (*' b a)
+               (unchecked-multiply a b) (unchecked-multiply b a)))
+    (assert (= (*' a b) (*' b a)))))
+
+(defspec integer-associative-laws
+  (partial map identity)
+  [^{:tag `integer} a ^{:tag `integer} b ^{:tag `integer} c]
+  (if (every? longable? [(+' a b) (+' b c) (+' a b c)])
+    (assert (= (+ (+ a b) c) (+ a (+ b c))
+               (+' (+' a b) c) (+' a (+' b c))
+               (unchecked-add (unchecked-add a b) c) (unchecked-add a (unchecked-add b c))))
+    (assert (= (+' (+' a b) c) (+' a (+' b c))
+               (+ (+ (bigint a) b) c) (+ a (+ (bigint b) c)))))
+  (if (every? longable? [(*' a b) (*' b c) (*' a b c)])
+    (assert (= (* (* a b) c) (* a (* b c))
+               (*' (*' a b) c) (*' a (*' b c))
+               (unchecked-multiply (unchecked-multiply a b) c) (unchecked-multiply a (unchecked-multiply b c))))
+    (assert (= (*' (*' a b) c) (*' a (*' b c))
+               (* (* (bigint a) b) c) (* a (* (bigint b) c))))))
+
+(defspec integer-distributive-laws
+  (partial map identity)
+  [^{:tag `integer} a ^{:tag `integer} b ^{:tag `integer} c]
+  (if (every? longable? [(*' a (+' b c)) (+' (*' a b) (*' a c))
+                         (*' a b) (*' a c) (+' b c)])
+    (assert (= (* a (+ b c)) (+ (* a b) (* a c))
+               (*' a (+' b c)) (+' (*' a b) (*' a c))
+               (unchecked-multiply a (+' b c)) (+' (unchecked-multiply a b) (unchecked-multiply a c))))
+    (assert (= (*' a (+' b c)) (+' (*' a b) (*' a c))
+               (* a (+ (bigint b) c)) (+ (* (bigint a) b) (* (bigint a) c))))))
+
+(defspec addition-undoes-subtraction
+  (partial map identity)
+  [^{:tag `integer} a ^{:tag `integer} b]
+  (if (longable? (-' a b))
+    (assert (= a
+               (-> a (- b) (+ b))
+               (-> a (unchecked-subtract b) (unchecked-add b)))))
+  (assert (= a
+             (-> a (-' b) (+' b)))))
+
+(defspec quotient-and-remainder
+  (fn [a b] (sort [a b]))
+  [^{:tag `integer} a ^{:tag `integer} b]
+  (when-not (zero? (second %))
+    (let [[a d] %
+          q (quot a d)
+          r (rem a d)]
+      (assert (= a
+                 (+ (* q d) r)
+                 (unchecked-add (unchecked-multiply q d) r))))))
