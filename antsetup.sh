a61a7031ec7a21852cc93cb1a1d16c14aaedbc2c
Stuart Halloway <stu@thinkrelevance.com>
2012-08-21 23:17:35 -0400

    add test.generative as a test dependency
    Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

--- antsetup.sh
+++ antsetup.sh
@@ -3,5 +3,6 @@
 mvn -q dependency:build-classpath -Dmdep.outputFile=maven-classpath
 cat <<EOF >maven-classpath.properties 
 maven.compile.classpath=`cat maven-classpath`
+maven.test.classpath=`cat maven-classpath`
 EOF
 echo "Wrote maven-classpath.properties for standalone ant use"
