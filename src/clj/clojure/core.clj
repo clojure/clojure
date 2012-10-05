848aa20b45b01cfa6d5aacff4999528261c187ef
Kirk Kimmel <kimmel.k.programmer@gmail.com>
2012-10-05 00:44:45 -0400

    Spelling correction.

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -2807,7 +2807,7 @@
   "Returns a lazy sequence of lists of n items each, at offsets step
   apart. If step is not supplied, defaults to n, i.e. the partitions
   do not overlap. If a pad collection is supplied, use its elements as
-  necessary to complete last partition upto n items. In case there are
+  necessary to complete last partition up to n items. In case there are
   not enough padding elements, return a partition with less than n items."
   {:added "1.0"
    :static true}
