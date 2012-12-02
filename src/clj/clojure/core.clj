372f03e2fa63ff7c3544be82d85e8943e85e640b
Rich Hickey <richhickey@gmail.com>
2012-12-02 15:18:01 -0500

    renames: cond->, as->, some->

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -6725,12 +6725,13 @@
   {:added "1.3"}
   [^clojure.lang.IPending x] (.isRealized x))
 
-(defmacro test->
+(defmacro cond->
   "Takes an expression and a set of test/form pairs. Threads expr (via ->)
-  through each form for which the corresponding test expression (not threaded) is true."
+  through each form for which the corresponding test
+  expression is true. Note that, unlike cond branching, cond-> threading does
+  not short circuit after the first true test expression."
   {:added "1.5"}
-  [expr
-  & clauses]
+  [expr & clauses]
   (assert (even? (count clauses)))
   (let [g (gensym)
         pstep (fn [[test step]] `(if ~test (-> ~g ~step) ~g))]
@@ -6738,9 +6739,11 @@
            ~@(interleave (repeat g) (map pstep (partition 2 clauses)))]
        ~g)))
 
-(defmacro test->>
+(defmacro cond->>
   "Takes an expression and a set of test/form pairs. Threads expr (via ->>)
-  through each form for which the corresponding test expression (not threaded) is true."
+  through each form for which the corresponding test expression
+  is true.  Note that, unlike cond branching, cond->> threading does not short circuit
+  after the first true test expression."
   {:added "1.5"}
   [expr & clauses]
   (assert (even? (count clauses)))
@@ -6750,34 +6753,34 @@
            ~@(interleave (repeat g) (map pstep (partition 2 clauses)))]
        ~g)))
 
-(defmacro let->
+(defmacro as->
   "Binds name to expr, evaluates the first form in the lexical context
   of that binding, then binds name to that result, repeating for each
-  successive form"
+  successive form, returning the result of the last form."
   {:added "1.5"}
   [expr name & forms]
   `(let [~name ~expr
          ~@(interleave (repeat name) forms)]
      ~name))
 
-(defmacro when->
-  "When expr is logical true, threads it into the first form (via ->),
-  and when that result is logical true, through the next etc"
+(defmacro some->
+  "When expr is not nil, threads it into the first form (via ->),
+  and when that result is not nil, through the next etc"
   {:added "1.5"}
   [expr & forms]
   (let [g (gensym)
-        pstep (fn [step] `(when ~g (-> ~g ~step)))]
+        pstep (fn [step] `(if (nil? ~g) nil (-> ~g ~step)))]
     `(let [~g ~expr
            ~@(interleave (repeat g) (map pstep forms))]
        ~g)))
 
-(defmacro when->>
-  "When expr is logical true, threads it into the first form (via ->>),
-  and when that result is logical true, through the next etc"
+(defmacro some->>
+  "When expr is not nil, threads it into the first form (via ->>),
+  and when that result is not nil, through the next etc"
   {:added "1.5"}
   [expr & forms]
   (let [g (gensym)
-        pstep (fn [step] `(when ~g (->> ~g ~step)))]
+        pstep (fn [step] `(if (nil? ~g) nil (->> ~g ~step)))]
     `(let [~g ~expr
            ~@(interleave (repeat g) (map pstep forms))]
        ~g)))
