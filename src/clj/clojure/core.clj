aca604afa463c96a8e094a3886d096c176865dcf
nickgieschen <nickgieschen@gmail.com>
2013-08-25 14:52:29 -0700

    Make mapcat not indicate that it can accept a variable number of colls

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -2519,7 +2519,7 @@
   to f and colls.  Thus function f should return a collection."
   {:added "1.0"
    :static true}
-  [f & colls]
+  [f colls]
     (apply concat (apply map f colls)))
 
 (defn filter
