f7215fdb8ca7d23faa7b47865d5acab1af8437a3
Devin Walters <devinw@gmail.com>
2013-11-22 12:45:01 -0600

    Add record? via IRecord
    Signed-off-by: Stuart Halloway <stu@cognitect.com>

--- src/clj/clojure/core_deftype.clj
+++ src/clj/clojure/core_deftype.clj
@@ -369,7 +369,14 @@
          ([m#] (~(symbol (str classname "/create")) m#)))
        ~classname)))
 
-(defn- emit-deftype* 
+(defn record?
+  "Returns true if x is a record"
+  {:added "1.6"
+   :static true}
+  [x]
+  (instance? clojure.lang.IRecord x))
+
+(defn- emit-deftype*
   "Do not use this directly - use deftype"
   [tagname name fields interfaces methods]
   (let [classname (with-meta (symbol (str (namespace-munge *ns*) "." name)) (meta name))
