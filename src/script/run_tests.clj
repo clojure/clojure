bb0a51a9fd5d589962b058855d7f0212d0f3c7a1
Alex Miller <alex.miller@cognitect.com>
2014-02-25 11:32:59 -0600

    CLJ-1353: Make awt run headless in the build.

--- src/script/run_tests.clj
+++ src/script/run_tests.clj
@@ -1,3 +1,4 @@
 (System/setProperty "clojure.test.generative.msec" "60000")
+(System/setProperty "java.awt.headless" "true")
 (require '[clojure.test.generative.runner :as runner])
 (runner/-main "test")
