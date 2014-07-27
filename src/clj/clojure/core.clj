9aaee3c111ce18b9b70695dd45a04b401a174113
Bozhidar Batsov <bozhidar@batsov.com>
2014-07-27 14:46:11 +0300

    Fix param name reference in defmulti's docstring
    Signed-off-by: Stuart Halloway <stu@cognitect.com>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -1607,7 +1607,7 @@
 
 (defmacro defmulti
   "Creates a new multimethod with the associated dispatch function.
-  The docstring and attribute-map are optional.
+  The docstring and attr-map are optional.
 
   Options are key-value pairs and may be one of:
 
