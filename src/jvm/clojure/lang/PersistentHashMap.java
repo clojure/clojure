46be47c9f51ef10d0082f1bd39ffff1008682861
Alex Miller <alex.miller@cognitect.com>
2014-09-03 14:03:02 -0500

    CLJ-1518 rollback owner type change from CLJ-1498
    Signed-off-by: Stuart Halloway <stu@cognitect.com>

--- src/jvm/clojure/lang/PersistentHashMap.java
+++ src/jvm/clojure/lang/PersistentHashMap.java
@@ -13,7 +13,7 @@ package clojure.lang;
 import java.io.Serializable;
 import java.util.*;
 import java.util.concurrent.Callable;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 /*
  A persistent rendition of Phil Bagwell's Hash Array Mapped Trie
@@ -238,7 +238,7 @@ public IPersistentMap meta(){
 }
 
 static final class TransientHashMap extends ATransientMap {
-	AtomicBoolean edit;
+	AtomicReference<Thread> edit;
 	INode root;
 	int count;
 	boolean hasNull;
@@ -247,10 +247,10 @@ static final class TransientHashMap extends ATransientMap {
 
 
 	TransientHashMap(PersistentHashMap m) {
-		this(new AtomicBoolean(true), m.root, m.count, m.hasNull, m.nullValue);
+		this(new AtomicReference<Thread>(Thread.currentThread()), m.root, m.count, m.hasNull, m.nullValue);
 	}
 	
-	TransientHashMap(AtomicBoolean edit, INode root, int count, boolean hasNull, Object nullValue) {
+	TransientHashMap(AtomicReference<Thread> edit, INode root, int count, boolean hasNull, Object nullValue) {
 		this.edit = edit;
 		this.root = root; 
 		this.count = count; 
@@ -297,7 +297,7 @@ static final class TransientHashMap extends ATransientMap {
 	}
 
 	IPersistentMap doPersistent() {
-		edit.set(false);
+		edit.set(null);
 		return new PersistentHashMap(count, root, hasNull, nullValue);
 	}
 
@@ -317,9 +317,8 @@ static final class TransientHashMap extends ATransientMap {
 	}
 	
 	void ensureEditable(){
-		if(edit.get())
-			return;
-		throw new IllegalAccessError("Transient used after persistent! call");
+		if(edit.get() == null)
+			throw new IllegalAccessError("Transient used after persistent! call");
 	}
 }
 
@@ -334,9 +333,9 @@ static interface INode extends Serializable {
 
 	ISeq nodeSeq();
 
-	INode assoc(AtomicBoolean edit, int shift, int hash, Object key, Object val, Box addedLeaf);
+	INode assoc(AtomicReference<Thread> edit, int shift, int hash, Object key, Object val, Box addedLeaf);
 
-	INode without(AtomicBoolean edit, int shift, int hash, Object key, Box removedLeaf);
+	INode without(AtomicReference<Thread> edit, int shift, int hash, Object key, Box removedLeaf);
 
     public Object kvreduce(IFn f, Object init);
 
@@ -346,9 +345,9 @@ static interface INode extends Serializable {
 final static class ArrayNode implements INode{
 	int count;
 	final INode[] array;
-	final AtomicBoolean edit;
+	final AtomicReference<Thread> edit;
 
-	ArrayNode(AtomicBoolean edit, int count, INode[] array){
+	ArrayNode(AtomicReference<Thread> edit, int count, INode[] array){
 		this.array = array;
 		this.edit = edit;
 		this.count = count;
@@ -459,20 +458,20 @@ final static class ArrayNode implements INode{
 	}
 
 
-	private ArrayNode ensureEditable(AtomicBoolean edit){
+	private ArrayNode ensureEditable(AtomicReference<Thread> edit){
 		if(this.edit == edit)
 			return this;
 		return new ArrayNode(edit, count, this.array.clone());
 	}
 	
-	private ArrayNode editAndSet(AtomicBoolean edit, int i, INode n){
+	private ArrayNode editAndSet(AtomicReference<Thread> edit, int i, INode n){
 		ArrayNode editable = ensureEditable(edit);
 		editable.array[i] = n;
 		return editable;
 	}
 
 
-	private INode pack(AtomicBoolean edit, int idx) {
+	private INode pack(AtomicReference<Thread> edit, int idx) {
 		Object[] newArray = new Object[2*(count - 1)];
 		int j = 1;
 		int bitmap = 0;
@@ -491,7 +490,7 @@ final static class ArrayNode implements INode{
 		return new BitmapIndexedNode(edit, bitmap, newArray);
 	}
 
-	public INode assoc(AtomicBoolean edit, int shift, int hash, Object key, Object val, Box addedLeaf){
+	public INode assoc(AtomicReference<Thread> edit, int shift, int hash, Object key, Object val, Box addedLeaf){
 		int idx = mask(hash, shift);
 		INode node = array[idx];
 		if(node == null) {
@@ -505,7 +504,7 @@ final static class ArrayNode implements INode{
 		return editAndSet(edit, idx, n);
 	}	
 
-	public INode without(AtomicBoolean edit, int shift, int hash, Object key, Box removedLeaf){
+	public INode without(AtomicReference<Thread> edit, int shift, int hash, Object key, Box removedLeaf){
 		int idx = mask(hash, shift);
 		INode node = array[idx];
 		if(node == null)
@@ -571,13 +570,13 @@ final static class BitmapIndexedNode implements INode{
 	
 	int bitmap;
 	Object[] array;
-	final AtomicBoolean edit;
+	final AtomicReference<Thread> edit;
 
 	final int index(int bit){
 		return Integer.bitCount(bitmap & (bit - 1));
 	}
 
-	BitmapIndexedNode(AtomicBoolean edit, int bitmap, Object[] array){
+	BitmapIndexedNode(AtomicReference<Thread> edit, int bitmap, Object[] array){
 		this.bitmap = bitmap;
 		this.array = array;
 		this.edit = edit;
@@ -696,7 +695,7 @@ final static class BitmapIndexedNode implements INode{
 		return NodeSeq.kvreduce(array, reducef, combinef.invoke());
 	}
 
-	private BitmapIndexedNode ensureEditable(AtomicBoolean edit){
+	private BitmapIndexedNode ensureEditable(AtomicReference<Thread> edit){
 		if(this.edit == edit)
 			return this;
 		int n = Integer.bitCount(bitmap);
@@ -705,20 +704,20 @@ final static class BitmapIndexedNode implements INode{
 		return new BitmapIndexedNode(edit, bitmap, newArray);
 	}
 	
-	private BitmapIndexedNode editAndSet(AtomicBoolean edit, int i, Object a) {
+	private BitmapIndexedNode editAndSet(AtomicReference<Thread> edit, int i, Object a) {
 		BitmapIndexedNode editable = ensureEditable(edit);
 		editable.array[i] = a;
 		return editable;
 	}
 
-	private BitmapIndexedNode editAndSet(AtomicBoolean edit, int i, Object a, int j, Object b) {
+	private BitmapIndexedNode editAndSet(AtomicReference<Thread> edit, int i, Object a, int j, Object b) {
 		BitmapIndexedNode editable = ensureEditable(edit);
 		editable.array[i] = a;
 		editable.array[j] = b;
 		return editable;
 	}
 
-	private BitmapIndexedNode editAndRemovePair(AtomicBoolean edit, int bit, int i) {
+	private BitmapIndexedNode editAndRemovePair(AtomicReference<Thread> edit, int bit, int i) {
 		if (bitmap == bit) 
 			return null;
 		BitmapIndexedNode editable = ensureEditable(edit);
@@ -729,7 +728,7 @@ final static class BitmapIndexedNode implements INode{
 		return editable;
 	}
 
-	public INode assoc(AtomicBoolean edit, int shift, int hash, Object key, Object val, Box addedLeaf){
+	public INode assoc(AtomicReference<Thread> edit, int shift, int hash, Object key, Object val, Box addedLeaf){
 		int bit = bitpos(hash, shift);
 		int idx = index(bit);
 		if((bitmap & bit) != 0) {
@@ -789,7 +788,7 @@ final static class BitmapIndexedNode implements INode{
 		}
 	}
 
-	public INode without(AtomicBoolean edit, int shift, int hash, Object key, Box removedLeaf){
+	public INode without(AtomicReference<Thread> edit, int shift, int hash, Object key, Box removedLeaf){
 		int bit = bitpos(hash, shift);
 		if((bitmap & bit) == 0)
 			return this;
@@ -820,9 +819,9 @@ final static class HashCollisionNode implements INode{
 	final int hash;
 	int count;
 	Object[] array;
-	final AtomicBoolean edit;
+	final AtomicReference<Thread> edit;
 
-	HashCollisionNode(AtomicBoolean edit, int hash, int count, Object... array){
+	HashCollisionNode(AtomicReference<Thread> edit, int hash, int count, Object... array){
 		this.edit = edit;
 		this.hash = hash;
 		this.count = count;
@@ -897,7 +896,7 @@ final static class HashCollisionNode implements INode{
 		return -1;
 	}
 
-	private HashCollisionNode ensureEditable(AtomicBoolean edit){
+	private HashCollisionNode ensureEditable(AtomicReference<Thread> edit){
 		if(this.edit == edit)
 			return this;
 		Object[] newArray = new Object[2*(count+1)]; // make room for next assoc
@@ -905,7 +904,7 @@ final static class HashCollisionNode implements INode{
 		return new HashCollisionNode(edit, hash, count, newArray);
 	}
 
-	private HashCollisionNode ensureEditable(AtomicBoolean edit, int count, Object[] array){
+	private HashCollisionNode ensureEditable(AtomicReference<Thread> edit, int count, Object[] array){
 		if(this.edit == edit) {
 			this.array = array;
 			this.count = count;
@@ -914,13 +913,13 @@ final static class HashCollisionNode implements INode{
 		return new HashCollisionNode(edit, hash, count, array);
 	}
 
-	private HashCollisionNode editAndSet(AtomicBoolean edit, int i, Object a) {
+	private HashCollisionNode editAndSet(AtomicReference<Thread> edit, int i, Object a) {
 		HashCollisionNode editable = ensureEditable(edit);
 		editable.array[i] = a;
 		return editable;
 	}
 
-	private HashCollisionNode editAndSet(AtomicBoolean edit, int i, Object a, int j, Object b) {
+	private HashCollisionNode editAndSet(AtomicReference<Thread> edit, int i, Object a, int j, Object b) {
 		HashCollisionNode editable = ensureEditable(edit);
 		editable.array[i] = a;
 		editable.array[j] = b;
@@ -928,7 +927,7 @@ final static class HashCollisionNode implements INode{
 	}
 
 
-	public INode assoc(AtomicBoolean edit, int shift, int hash, Object key, Object val, Box addedLeaf){
+	public INode assoc(AtomicReference<Thread> edit, int shift, int hash, Object key, Object val, Box addedLeaf){
 		if(hash == this.hash) {
 			int idx = findIndex(key);
 			if(idx != -1) {
@@ -954,7 +953,7 @@ final static class HashCollisionNode implements INode{
 			.assoc(edit, shift, hash, key, val, addedLeaf);
 	}	
 
-	public INode without(AtomicBoolean edit, int shift, int hash, Object key, Box removedLeaf){
+	public INode without(AtomicReference<Thread> edit, int shift, int hash, Object key, Box removedLeaf){
 		int idx = findIndex(key);
 		if(idx == -1)
 			return this;
@@ -1088,13 +1087,13 @@ private static INode createNode(int shift, Object key1, Object val1, int key2has
 	if(key1hash == key2hash)
 		return new HashCollisionNode(null, key1hash, 2, new Object[] {key1, val1, key2, val2});
 	Box addedLeaf = new Box(null);
-	AtomicBoolean edit = new AtomicBoolean(false);
+	AtomicReference<Thread> edit = new AtomicReference<Thread>();
 	return BitmapIndexedNode.EMPTY
 		.assoc(edit, shift, key1hash, key1, val1, addedLeaf)
 		.assoc(edit, shift, key2hash, key2, val2, addedLeaf);
 }
 
-private static INode createNode(AtomicBoolean edit, int shift, Object key1, Object val1, int key2hash, Object key2, Object val2) {
+private static INode createNode(AtomicReference<Thread> edit, int shift, Object key1, Object val1, int key2hash, Object key2, Object val2) {
 	int key1hash = hash(key1);
 	if(key1hash == key2hash)
 		return new HashCollisionNode(null, key1hash, 2, new Object[] {key1, val1, key2, val2});
--- src/jvm/clojure/lang/PersistentVector.java
+++ src/jvm/clojure/lang/PersistentVector.java
@@ -15,26 +15,26 @@ package clojure.lang;
 import java.io.Serializable;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class PersistentVector extends APersistentVector implements IObj, IEditableCollection{
 
 public static class Node implements Serializable {
-	transient public final AtomicBoolean edit;
+	transient public final AtomicReference<Thread> edit;
 	public final Object[] array;
 
-	public Node(AtomicBoolean edit, Object[] array){
+	public Node(AtomicReference<Thread> edit, Object[] array){
 		this.edit = edit;
 		this.array = array;
 	}
 
-	Node(AtomicBoolean edit){
+	Node(AtomicReference<Thread> edit){
 		this.edit = edit;
 		this.array = new Object[32];
 	}
 }
 
-final static AtomicBoolean NOEDIT = new AtomicBoolean(false);
+final static AtomicReference<Thread> NOEDIT = new AtomicReference<Thread>(null);
 public final static Node EMPTY_NODE = new Node(NOEDIT, new Object[32]);
 
 final int cnt;
@@ -215,7 +215,7 @@ private Node pushTail(int level, Node parent, Node tailnode){
 	return ret;
 }
 
-private static Node newPath(AtomicBoolean edit,int level, Node node){
+private static Node newPath(AtomicReference<Thread> edit,int level, Node node){
 	if(level == 0)
 		return node;
 	Node ret = new Node(edit);
@@ -457,16 +457,15 @@ static final class TransientVector extends AFn implements ITransientVector, Coun
 	}
 
 	void ensureEditable(){
-		if(root.edit.get())
-			return;
-		throw new IllegalAccessError("Transient used after persistent! call");
+		if(root.edit.get() == null)
+			throw new IllegalAccessError("Transient used after persistent! call");
 
 //		root = editableRoot(root);
 //		tail = editableTail(tail);
 	}
 
 	static Node editableRoot(Node node){
-		return new Node(new AtomicBoolean(true), node.array.clone());
+		return new Node(new AtomicReference<Thread>(Thread.currentThread()), node.array.clone());
 	}
 
 	public PersistentVector persistent(){
@@ -476,7 +475,7 @@ static final class TransientVector extends AFn implements ITransientVector, Coun
 //			{
 //			throw new IllegalAccessError("Mutation release by non-owner thread");
 //			}
-		root.edit.set(false);
+		root.edit.set(null);
 		Object[] trimmedTail = new Object[cnt-tailoff()];
 		System.arraycopy(tail,0,trimmedTail,0,trimmedTail.length);
 		return new PersistentVector(cnt, shift, root, trimmedTail);
