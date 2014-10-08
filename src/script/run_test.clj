18bcde4d63e5a75c02cfe8d1574f2f26ee68d557
Alex Miller <alex.miller@cognitect.com>
2014-10-08 10:34:15 -0500

    update to latest test.generative, add test.check, reorganize test parts of build
    Signed-off-by: Stuart Halloway <stu@cognitect.com>

---
+++ src/script/run_test.clj
@@ -0,0 +1,8 @@
+(System/setProperty "java.awt.headless" "true")
+(require
+ '[clojure.test :as test]
+ '[clojure.tools.namespace :as ns])
+(def namespaces (ns/find-namespaces-in-dir (java.io.File. "test")))
+(doseq [ns namespaces] (require ns))
+(let [summary (apply test/run-tests namespaces)]
+  (System/exit (if (test/successful? summary) 0 -1)))
