c7bae3fcfb7865fe43a31a48b1e34483d5214c82
Blake <bwest87@gmail.com>
2015-05-03 17:21:36 -0700

    Fixed a typo in `with-bindings` doc string

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -1869,7 +1869,7 @@
 
 (defmacro with-bindings
   "Takes a map of Var/value pairs. Installs for the given Vars the associated
-  values as thread-local bindings. The executes body. Pops the installed
+  values as thread-local bindings. Then executes body. Pops the installed
   bindings after body was evaluated. Returns the value of body."
   {:added "1.1"}
   [binding-map & body]
