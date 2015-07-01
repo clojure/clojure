7754e94377042cc539182f48a66010a5e260e8ea
Daniel Compton <desk@danielcompton.net>
2015-07-01 17:40:38 +1200

    CLJ-1772 Fix spelling in clojure.test/use-fixtures
    Signed-off-by: Stuart Halloway <stu@cognitect.com>

--- src/clj/clojure/test.clj
+++ src/clj/clojure/test.clj
@@ -656,7 +656,7 @@
 (defmulti use-fixtures
   "Wrap test runs in a fixture function to perform setup and
   teardown. Using a fixture-type of :each wraps every test
-  individually, while:once wraps the whole run in a single function."
+  individually, while :once wraps the whole run in a single function."
   {:added "1.1"}
   (fn [fixture-type & args] fixture-type))
 
