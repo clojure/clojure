c3be0027c368a39372df48649773b5beb58ce9c6
Timothy Baldridge <tbaldridge@gmail.com>
2015-10-09 08:20:36 -0500

    CLJ-668 improve performance of slurp via jio/copy
    Signed-off-by: Stuart Halloway <stu@cognitect.com>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -6652,14 +6652,10 @@
   {:added "1.0"}
   ([f & opts]
      (let [opts (normalize-slurp-opts opts)
-           sb (StringBuilder.)]
+           sw (java.io.StringWriter.)]
        (with-open [^java.io.Reader r (apply jio/reader f opts)]
-         (loop [c (.read r)]
-           (if (neg? c)
-             (str sb)
-             (do
-               (.append sb (char c))
-               (recur (.read r)))))))))
+         (jio/copy r sw)
+         (.toString sw)))))
 
 (defn spit
   "Opposite of slurp.  Opens f with writer, writes content, then
