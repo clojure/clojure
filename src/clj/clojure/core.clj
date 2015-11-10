10a74b3fcd1df375b0d078429de293aabc4028d9
Alex Miller <alex.miller@cognitect.com>
2015-11-10 09:41:08 -0600

    CLJ-1845 Mark load as dynamic to allow redefinition
    Signed-off-by: Stuart Halloway <stu@cognitect.com>

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -5879,7 +5879,8 @@
   "Loads Clojure code from resources in classpath. A path is interpreted as
   classpath-relative if it begins with a slash or relative to the root
   directory for the current namespace otherwise."
-  {:added "1.0"}
+  {:dynamic true
+   :added "1.0"}
   [& paths]
   (doseq [^String path paths]
     (let [^String path (if (.startsWith path "/")
