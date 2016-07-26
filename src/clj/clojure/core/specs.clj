1c8a13a9a9dbcafda3168ab191ba6e5ce61c8f96
Alex Miller <alex.miller@cognitect.com>
2016-07-26 13:43:24 -0500

    specs for ns
    Signed-off-by: Rich Hickey <richhickey@gmail.com>

--- src/clj/clojure/core/specs.clj
+++ src/clj/clojure/core/specs.clj
@@ -96,3 +96,106 @@
                :bs (s/alt :arity-1 ::args+body
                           :arity-n (s/+ (s/spec ::args+body))))
   :ret any?)
+
+;;;; ns
+
+(s/def ::exclude (s/coll-of simple-symbol?))
+(s/def ::only (s/coll-of simple-symbol?))
+(s/def ::rename (s/map-of simple-symbol? simple-symbol?))
+
+(s/def ::ns-refer-clojure
+  (s/spec (s/cat :clause #{:refer-clojure}
+            :filters (s/keys* :opt-un [::exclude ::only ::rename]))))
+
+(s/def ::refer (s/or :all #{:all}
+                 :syms  (s/coll-of simple-symbol?)))
+
+(s/def ::prefix-list
+  (s/spec
+    (s/cat :prefix simple-symbol?
+      :suffix (s/* (s/alt :lib simple-symbol? :prefix-list ::prefix-list))
+      :refer (s/keys* :opt-un [::as ::refer]))))
+
+(s/def ::ns-require
+  (s/spec (s/cat :clause #{:require}
+            :libs (s/* (s/alt :lib simple-symbol?
+                         :prefix-list ::prefix-list
+                         :flag #{:reload :reload-all :verbose})))))
+
+(s/def ::package-list
+  (s/spec
+    (s/cat :package simple-symbol?
+      :classes (s/* simple-symbol?))))
+
+(s/def ::ns-import
+  (s/spec
+    (s/cat :clause #{:import}
+      :classes (s/* (s/alt :class simple-symbol?
+                      :package-list ::package-list)))))
+
+(s/def ::ns-refer
+  (s/spec (s/cat :clause #{:refer}
+            :lib simple-symbol?
+            :filters (s/keys* :opt-un [::exclude ::only ::rename]))))
+
+(s/def ::use-prefix-list
+  (s/spec
+    (s/cat :prefix simple-symbol?
+      :suffix (s/* (s/alt :lib simple-symbol? :prefix-list ::use-prefix-list))
+      :filters (s/keys* :opt-un [::exclude ::only ::rename]))))
+
+(s/def ::ns-use
+  (s/spec (s/cat :clause #{:use}
+            :libs (s/* (s/alt :lib simple-symbol?
+                         :prefix-list ::use-prefix-list
+                         :flag #{:reload :reload-all :verbose})))))
+
+(s/def ::ns-load
+  (s/spec (s/cat :clause #{:load}
+            :libs (s/* string?))))
+
+(s/def ::name simple-symbol?)
+(s/def ::extends simple-symbol?)
+(s/def ::implements (s/coll-of simple-symbol? :kind vector?))
+(s/def ::init symbol?)
+(s/def ::signature (s/coll-of simple-symbol? :kind vector?))
+(s/def ::constructors (s/map-of ::signature ::signature))
+(s/def ::post-init symbol?)
+(s/def ::method (s/and vector?
+                  (s/cat :name simple-symbol?
+                    :param-types ::signature
+                    :return-type simple-symbol?)))
+(s/def ::methods (s/coll-of ::method :kind vector?))
+(s/def ::main boolean?)
+(s/def ::factory simple-symbol?)
+(s/def ::state simple-symbol?)
+(s/def ::get simple-symbol?)
+(s/def ::set simple-symbol?)
+(s/def ::expose (s/keys :opt-un [::get ::set]))
+(s/def ::exposes (s/map-of simple-symbol? ::expose))
+(s/def ::prefix string?)
+(s/def ::impl-ns simple-symbol?)
+(s/def ::load-impl-ns boolean?)
+
+(s/def ::ns-gen-class
+  (s/spec (s/cat :clause #{:gen-class}
+            :options (s/keys* :opt-un [::name ::extends ::implements
+                                       ::init ::constructors ::post-init
+                                       ::methods ::main ::factory ::state
+                                       ::exposes ::prefix ::impl-ns ::load-impl-ns]))))
+
+(s/def ::ns-clauses
+  (s/* (s/alt :refer-clojure ::ns-refer-clojure
+         :require ::ns-require
+         :import ::ns-import
+         :use ::ns-use
+         :refer ::ns-refer
+         :load ::ns-load
+         :gen-class ::ns-gen-class)))
+
+(s/fdef clojure.core/ns
+  :args (s/cat :name simple-symbol?
+          :docstring (s/? string?)
+          :attr-map (s/? map?)
+          :clauses ::ns-clauses)
+  :ret any?)
