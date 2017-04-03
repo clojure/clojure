77ce459f6b83aa8513fd7084ee31f2c9930d178b
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2017-04-03 14:15:34 -0700

    Define the arguments of reduce’s callback function

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -6684,15 +6684,15 @@
   [x] (instance? java.util.UUID x))
 
 (defn reduce
-  "f should be a function of 2 arguments. If val is not supplied,
-  returns the result of applying f to the first 2 items in coll, then
-  applying f to that result and the 3rd item, etc. If coll contains no
-  items, f must accept no arguments as well, and reduce returns the
-  result of calling f with no arguments.  If coll has only 1 item, it
-  is returned and f is not called.  If val is supplied, returns the
-  result of applying f to val and the first item in coll, then
-  applying f to that result and the 2nd item, etc. If coll contains no
-  items, returns val and f is not called."
+  "f should be a function of 2 arguments: 1st, the accumulator, and 2nd the
+  current element. If val is not supplied, returns the result of applying f to
+  the first 2 items in coll, then applying f to that result and the 3rd item,
+  etc. If coll contains no items, f must accept no arguments as well, and reduce
+  returns the result of calling f with no arguments.  If coll has only 1 item,
+  it is returned and f is not called.  If val is supplied, returns the result of
+  applying f to val and the first item in coll, then applying f to that result
+  and the 2nd item, etc. If coll contains no items, returns val and f is not
+  called."
   {:added "1.0"}
   ([f coll]
      (if (instance? clojure.lang.IReduce coll)
