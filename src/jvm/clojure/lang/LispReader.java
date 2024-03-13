45c91f7a11659aaa2af519d6c0b7c02fec603258
fogus <mefogus@gmail.com>
2024-03-13 16:06:23 -0400

    combining trailing colon and double colon checks

--- src/jvm/clojure/lang/LispReader.java
+++ src/jvm/clojure/lang/LispReader.java
@@ -421,10 +421,9 @@ private static Object matchSymbol(String s, Resolver resolver){
 		String name = m.group(2);
 		boolean isKeyword = s.charAt(0) == ':';
 
-		if(ns != null && ns.endsWith(":/") || name.endsWith(":"))
-			return null;
-		else if(s.indexOf("::", 1) != -1 &&
-				(isKeyword || ns != null || Util.maybeArrayComponents(name) == null))
+		if((ns != null && ns.endsWith(":/") || name.endsWith(":")) ||
+			(s.indexOf("::", 1) != -1 &&
+				(isKeyword || ns != null || Util.maybeArrayComponents(name) == null)))
 			return null;
 		if(s.startsWith("::"))
 			{
