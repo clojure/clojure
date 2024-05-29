74566184ee699b8808bc0240f52f902c5f6f465f
Fogus <mefogus@gmail.com>
2024-05-29 10:26:10 -0400

    blacklisting prims for ret hinting.

--- src/clj/clojure/core_deftype.clj
+++ src/clj/clojure/core_deftype.clj
@@ -654,13 +654,14 @@
         sigs (when sigs
                (reduce1 (fn [m s]
                           (let [tag-to-class (fn [tag]
-                                               (if-let [c (and (instance? clojure.lang.Symbol tag)
-                                                            (= (.indexOf (.getName ^clojure.lang.Symbol tag) ".") -1)
-                                                            (not (contains? '#{int long float double char short byte boolean void
-                                                                               ints longs floats doubles chars shorts bytes booleans objects} tag))
-                                                            (resolve tag))]
-                                                 (symbol (.getName c))
-                                                 tag))
+                                               (let [prim-blacklist '#{int long float double char short byte boolean void
+                                                                       ints longs floats doubles chars shorts bytes booleans objects}]
+                                                 (if-let [c (and (instance? clojure.lang.Symbol tag)
+                                                                 (= (.indexOf (.getName ^clojure.lang.Symbol tag) ".") -1)
+                                                                 (not (contains? prim-blacklist tag))
+                                                                 (resolve tag))]
+                                                   (symbol (.getName c))
+                                                   (when (not (contains? prim-blacklist tag)) tag))))
                                 name-meta (update-in (meta (first s)) [:tag] tag-to-class)
                                 mname (with-meta (first s) nil)
                                 [arglists doc]
