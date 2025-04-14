afbd2098bf741234ec9efaa25480d02b14863e8c
Alex Miller <alex.miller@cognitect.com>
2025-04-14 15:32:48 -0500

    CLJ-2906 Add missing :metadata to new functions in 1.12

--- src/clj/clojure/java/process.clj
+++ src/clj/clojure/java/process.clj
@@ -95,22 +95,26 @@
 
 (defn stdin
   "Given a process, return the stdin of the external process (an OutputStream)"
+  {:added "1.12"}
   ^OutputStream [^Process process]
   (.getOutputStream process))
 
 (defn stdout
   "Given a process, return the stdout of the external process (an InputStream)"
+  {:added "1.12"}
   ^InputStream [^Process process]
   (.getInputStream process))
 
 (defn stderr
   "Given a process, return the stderr of the external process (an InputStream)"
+  {:added "1.12"}
   ^InputStream [^Process process]
   (.getErrorStream process))
 
 (defn exit-ref
   "Given a Process (the output of 'start'), return a reference that can be
   used to wait for process completion then returns the exit value."
+  {:added "1.12"}
   [^Process process]
   (reify
     IDeref
