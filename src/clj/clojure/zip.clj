13b2a2014fda5d4d1c5a8a9d52129813d11bf95d
Alex Miller <alex.miller@cognitect.com>
2025-11-25 14:30:56 -0600

    CLJ-2929 zipper - fix typo in docstring
    Signed-off-by: Alex Miller <alex.miller@cognitect.com>

--- src/clj/clojure/zip.clj
+++ src/clj/clojure/zip.clj
@@ -18,7 +18,7 @@
 (defn zipper
   "Creates a new zipper structure. 
 
-  branch? is a fn that, given a node, returns true if can have
+  branch? is a fn that, given a node, returns true if it can have
   children, even if it currently doesn't.
 
   children is a fn that, given a branch node, returns a seq of its
