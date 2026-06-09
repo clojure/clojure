dd13fcb652d6744425c97757c47e48b7f1941cf8
Rafal Szalanski <rafal.szalanski@gmail.com>
2026-06-09 17:30:32 -0500

    CLJ-1899 Add transform-keys to refactor keywordize-keys and stringify-keys
    Signed-off-by: Alex Miller <alex.miller@cognitect.com>

--- src/clj/clojure/walk.clj
+++ src/clj/clojure/walk.clj
@@ -91,21 +91,25 @@ the sorting function."}
   [form]
   (prewalk (fn [x] (print "Walked: ") (prn x) x) form))
 
+(defn transform-keys
+  "Recursively transform all map keys using f as a transformation function."
+  {:added "1.13"}
+  [m f]
+  (let [entry-f (fn [[k v]] [(f k) v])]
+    ;; only apply to maps
+    (postwalk (fn [x] (if (map? x) (into {} (map entry-f x)) x)) m)))
+
 (defn keywordize-keys
   "Recursively transforms all map keys from strings to keywords."
   {:added "1.1"}
   [m]
-  (let [f (fn [[k v]] (if (string? k) [(keyword k) v] [k v]))]
-    ;; only apply to maps
-    (postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
+  (transform-keys m (fn [k] (if (string? k) (keyword k) k))))
 
 (defn stringify-keys
   "Recursively transforms all map keys from keywords to strings."
   {:added "1.1"}
   [m]
-  (let [f (fn [[k v]] (if (keyword? k) [(name k) v] [k v]))]
-    ;; only apply to maps
-    (postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
+  (transform-keys m (fn [k] (if (keyword? k) (name k) k))))
 
 (defn prewalk-replace
   "Recursively transforms form by replacing keys in smap with their
